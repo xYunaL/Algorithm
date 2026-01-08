7699496b12d27c35cdef1d7db7b5ace5a7af5d3c
xMeowtch <lys8167@gmail.com>
2026-01-08 20:35:53 +0900

    add: #1330, #9498 solve

---
+++ baekjoon/Bronze/B5_1330.java
@@ -0,0 +1,17 @@
+package baekjoon.Bronze;
+
+import java.util.*;
+import java.io.*;
+
+public class B5_1330 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int first = Integer.parseInt(st.nextToken());
+        int second = Integer.parseInt(st.nextToken());
+        if (first>second) System.out.println(">");
+        else if (first<second) System.out.println("<");
+        else System.out.println("==");
+        br.close();
+    }
+}
