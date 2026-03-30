53b3837b4cefd9ebdc8f5921449a85a78c320c23
yoonseo <lys8167@gmail.com>
2026-03-30 13:18:00 +0900

    add: 오랜 기간 보호한 동물(2)

---
+++ programmers/SQL/lv3/오랜 기간 보호한 동물(2).sql
@@ -0,0 +1,7 @@
+-- 입양을 간 동물 중, 보호 기간이 가장 길었던 동물 두 마리의 아이디와 이름을 조회
+-- 보호 기간이 긴 순으로 조회
+SELECT outs.ANIMAL_ID, outs.NAME
+FROM ANIMAL_OUTS outs INNER JOIN ANIMAL_INS ins
+    ON ins.ANIMAL_ID = outs.ANIMAL_ID
+ORDER BY (outs.DATETIME - ins.DATETIME) DESC
+LIMIT 2;
